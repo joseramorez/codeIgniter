4e454e6042adc71cfa377873e8e433656dcbf513
joseramorez <joseramorez.jr@gmail.com>
2018-10-22 15:31:52 -0500

    jose
    sad

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,8 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -271,7 +272,8 @@ $config['log_file_permissions'] = 0644;
 | codes to set your own date formatting
 |
 */
-$config['log_date_format'] = 'Y-m-d H:i:s';
+// $config['log_date_format'] = 'Y-m-d H:i:s';
+$config['log_date_format'] = 'd-m-Y H:i:s';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/views/login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- <link rel="stylesheet" type="text/css" href="/static/css/login.css" /> -->
+    <link rel="stylesheet" href="/static/css/alertify.css">
+    <link rel="stylesheet" href="/static/css/themes/bootstrap.css">
+    <link rel="stylesheet" href="/static/css/font-awesome.css">
+    <link rel="stylesheet" href="/static/css/bootstrap.css">
+    <link rel="stylesheet" href="/static/css/main.css">
+    <link rel="stylesheet" href="/static/css/util.css">
+
+    <title>Ingreso al Sistema</title>
+
+</head>
+
+<body>
+    <script src="/static/js/jquery-3.1.1.js"></script>
+    <script src="/static/js/jquery.validate.js"></script>
+    <script src="/static/js/alertify.js"></script>
+    <script type="text/javascript">
+        alertify.defaults.transition = "slide";
+        alertify.defaults.theme.ok = "btn btn-primary";
+        alertify.defaults.theme.cancel = "btn btn-danger";
+        alertify.defaults.theme.input = "form-control";
+    </script>
+
+    <div class="limiter">
+  		<div class="container-login100">
+  			<div class="wrap-login100">
+  				<div class="login100-form-title" style="background-image: url(/static/img/dreamsboys.jpg);">
+  					<span class="login100-form-title-1">
+  						INICIAR SESSION
+  					</span>
+  				</div>
+
+  				<form class="login100-form validate-form"  method="POST" action="/users/user/check" id="login">
+  					<div class="wrap-input100 validate-input m-b-26" data-validate="Usuario requerido">
+  						<span class="label-input100">Usuario</span>
+  						<input class="input100" type="text" name="user" id="user" placeholder="DreamsBoys">
+  						<span class="focus-input100"></span>
+  					</div>
+
+  					<div class="wrap-input100 validate-input m-b-18" data-validate = "Contraseña requerido">
+  						<span class="label-input100">Contraseña</span>
+  						<input class="input100" type="password" name="pwd" id="pwd" placeholder="*******">
+  						<span class="focus-input100"></span>
+  					</div>
+
+
+  					<div class="container-login100-form-btn">
+  						<button class="login100-form-btn">
+  							INICIAR SESSION
+  						</button>
+  					</div>
+  				</form>
+  			</div>
+  		</div>
+  	</div>
+
+
+    <script src="/static/js/main.js"></script>
+    <script src="/static/js/map-custom.js"></script>
+</body>
