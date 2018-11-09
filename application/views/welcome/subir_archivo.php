599086e5ab0e19028774d90ae061f86eb0e76778
joseramorez <joseramorez.jr@gmail.com>
2018-11-09 16:05:27 -0600

    jose

--- application/views/welcome/subir_archivo.php
+++ application/views/welcome/subir_archivo.php
@@ -3,13 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 <div class="row">
-  <div class="container-fluid">
-    <div class="col-md-4 col-md-offset-4">
+  <div class="container">
+    <div class="col col-md-4 offset-md-2">
       <form class="" action="subir" method="post" enctype="multipart/form-data">
-        <?php echo $error ?>
-        <div class="custom-file mb-3">
-         <input type="file" class="custom-file-input" id="customFile" name="archivo_subir" value="archivo">
-         <label class="custom-file-label" for="customFile">SUBIR</label>
+        <?php if ($error): ?>
+          <label for="" class="label label-error" ><?php echo $error ?></label>
+        <?php endif; ?>
+        <div class="form-group">
+         <label for="selecciona">Seleccione archivo</label>
+         <input type="file" class="form-control-file" name="archivo_subir" id="archivo_subir" >
        </div>
        <input type="submit" name="" value="ACEPTAR" class="btn btn-success">
       </form>
